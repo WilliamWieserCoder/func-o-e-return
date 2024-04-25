d50b0c7cbccfef98fb7d284c7ddcfe70e8cae867
William Wieser <166855277+C3bola99@users.noreply.github.com>
2024-04-25 16:57:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ function somarNumeros (n1, n2) {
 
 let resultado=somarNumeros(1,1)
 console.log(resultado)
+
+
+
+
+
+
+function transformaArray (vetor) {
+    let array2 = []
+    array2 = push(vetor[0])
+    array2.push(vetor[vetor.lenght-1])
+    return array2
+  
+  }
+  
+ let array = [1,2,3,4,5]
+ let novoArray = transformaArray(array)
